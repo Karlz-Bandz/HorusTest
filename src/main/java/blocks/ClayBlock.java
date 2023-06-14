621303d4a59a3dc2fbb45829d8627c7658079cea
Melak <km250542@ncr.com>
2023-06-14 13:01:02 +0200

    Methods implemented correctly

---
+++ src/main/java/blocks/ClayBlock.java
@@ -0,0 +1,11 @@
+package blocks;
+
+import composite.classes.ParentBlock;
+
+public class ClayBlock extends ParentBlock
+{
+    public ClayBlock(String color, String material)
+    {
+        super(color, material);
+    }
+}
--- src/main/java/composite/classes/Wall.java
+++ src/main/java/composite/classes/Wall.java
@@ -2,31 +2,43 @@ package composite.classes;
 
 import composite.interfaces.Block;
 import composite.interfaces.Structure;
-import lombok.AllArgsConstructor;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-@AllArgsConstructor
 public class Wall implements Structure
 {
     private List<Block> blocks;
 
+    public Wall(List<Block> blocks)
+    {
+        this.blocks = blocks;
+    }
+
     @Override
     public Optional<Block> findBlockByColor(String color)
     {
-        return Optional.empty();
+        Optional<Block> foundBlock = blocks.stream()
+                .filter(block -> color.equals(block.getColor()))
+                .findAny();
+
+        return foundBlock;
     }
 
     @Override
     public List<Block> findBlocksByMaterial(String material)
     {
-        return null;
+        List<Block> foundBlocks = blocks.stream()
+                .filter(block -> material.equals(block.getMaterial()))
+                .collect(Collectors.toList());
+
+        return foundBlocks;
     }
 
     @Override
     public int count()
     {
-        return 0;
+        return this.blocks.size();
     }
 }
