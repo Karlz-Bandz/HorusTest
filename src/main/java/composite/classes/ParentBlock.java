abbf8bd74e8fc88771d84c47f0eeede3b3607706
Melak <km250542@ncr.com>
2023-06-15 08:38:12 +0200

    Test passed

--- src/main/java/composite/classes/ParentBlock.java
+++ src/main/java/composite/classes/ParentBlock.java
@@ -1,12 +1,17 @@
 package composite.classes;
 
 import composite.interfaces.Block;
+import composite.interfaces.CompositeBlock;
 
-public class ParentBlock implements Block
+import java.util.List;
+
+public class ParentBlock implements CompositeBlock
 {
     protected String color;
 
     protected String material;
+
+    protected List<Block> blocks;
 
     public ParentBlock(String color, String material)
     {
@@ -24,5 +29,11 @@ public class ParentBlock implements Block
     public String getMaterial()
     {
         return this.material;
+    }
+
+    @Override
+    public List<Block> getBlocks()
+    {
+        return this.blocks;
     }
 }
